68289eb45e96595258c0293357ff67b0ce5d560a
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-18 20:22:40 +0900

    Add transition to the delete button
    todo item is dropped and removed when delete button is clicked

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,10 @@
 const elements = {
     todoInput: document.querySelector('.todo-input'),
     todoBtn: document.querySelector('.todo-btn'),
+    todoContainer: document.querySelector('.todo-container'),
     todoList: document.querySelector('.todo-list'),
     todoItem: document.querySelector('.todo'),
 };
-
 
 /**
  *  FUNCTIONS
@@ -20,8 +20,8 @@ const addTodo = e => {
     const markup = `
     <div class="todo">
         <li class="todo-item">${elements.todoInput.value}</li>
-        <button class="btn-complete btn"><i class="far fa-check-square" icon></i></button>
-        <button class="btn-delete btn"><i class="far fa-trash-alt" icon></i></button>
+        <button class="btn-complete btn"><i class="far fa-check-square icon" ></i></button>
+        <button class="btn-delete btn"><i class="far fa-trash-alt icon"></i></button>
     </div>
     `
     elements.todoList.insertAdjacentHTML('beforeend', markup);
@@ -41,21 +41,24 @@ const controller = e => {
 };
 
 
+
 /**
  * EVENT HANDLER
  */
 elements.todoBtn.addEventListener('click', controller);
 
 elements.todoList.addEventListener('click', e => {
-    const deleteBtn = e.target.closest('.btn-delete');
-    const completeBtn = e.target.closest('.btn-complete');
+    const item = e.target;
 
-    // Delete todo
-    if (deleteBtn) {
-        deleteBtn.parentElement.remove(elements.todoItem); 
+    if (e.target.matches('.btn-complete, .btn-complete *')) {
+        // Click complete button
+        item.parentElement.classList.toggle('completed');
 
-    // Complete todo
-    } else if (completeBtn) {
-        completeBtn.parentElement.classList.toggle('completed');
-    };
+      // Click remove button  
+    } else if (item.matches('.btn-delete, .btn-delete *')) {
+        item.parentElement.classList.add('fall');
+        item.parentElement.addEventListener('transitionend', e => {
+            item.remove();
+        });
+    }
 });
